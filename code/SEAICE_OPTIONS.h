571be3614f17b4c2835d8637712c1a6c9a3d5a07
hmason13 <hassancmason@gmail.com>
2024-09-18 11:45:06 -0400

    further updating plots after feedback

--- code/SEAICE_OPTIONS.h
+++ code/SEAICE_OPTIONS.h
@@ -4,17 +4,13 @@
 #include "CPP_OPTIONS.h"
 
 
-C hassan: from verification/offline_exf_seaice
-C hassan: maybe this will fix my issues?
 C By default, the sea-ice package uses its own integrated bulk
 C formulae to compute fluxes over open ocean. When this flag is set,
 C these variables are computed in a separate external package,
 C for example, pkg/exf, and then modified for sea-ice effects by
 C pkg/seaice.
-C turns out these are defaults...
 #define SEAICE_EXTERNAL_FLUXES
 
-C hassan: The same file says to add this as well
 #define SEAICE_CGRID
 
 #define SEAICE_ALLOW_DYNAMICS
